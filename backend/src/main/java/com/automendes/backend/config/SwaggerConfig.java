64226b5b4f2e9e9e4f0acf583a82cc6c70ee98fc
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-10-19 16:01:28 -0300

    Document API.

---
+++ backend/src/main/java/com/automendes/backend/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.automendes.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+            .title("API automendes.")
+            .version("1.0.0")
+            .description("Documentação da API automendes com a OpenAPI"));
+    }
+}
