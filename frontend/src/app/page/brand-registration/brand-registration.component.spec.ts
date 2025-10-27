cb82db88c0d900bf273d92166c19afb9ea2437fc
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-10-27 17:00:11 -0300

    Implement tailwind.

---
+++ frontend/src/app/page/brand-registration/brand-registration.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BrandRegistrationComponent } from './brand-registration.component';
+
+describe('BrandRegistrationComponent', () => {
+  let component: BrandRegistrationComponent;
+  let fixture: ComponentFixture<BrandRegistrationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BrandRegistrationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BrandRegistrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/page/brand-registration/brand-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-registration',
+  imports: [],
+  templateUrl: './brand-registration.component.html',
+  styleUrl: './brand-registration.component.css'
+})
+export class BrandRegistrationComponent {
+
+}
